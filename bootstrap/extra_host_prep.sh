7043e22ec02c95f17fccff7787c2e5505893e50e
John Gardner <huxoll@gmail.com>
2018-06-28 21:47:13 -0500

    Add script to allow gitlab checkouts.

--- bootstrap/extra_host_prep.sh
+++ bootstrap/extra_host_prep.sh
@@ -1,3 +1,27 @@
 #!/bin/bash
 
-echo "Do some stuff with host at $ip"
+echo
+echo "Setting up user $user at host $ip to connect to Gitlab"
+
+if [ -n "$GITLAB_SSH_KEY" ] && ! [ -f "$HOME/.ssh/id_rsa_gitlab" ]; then
+  echo "Expect a value for GITLAB_SSH_KEY or a key to exist at \
+  ~/.ssh/id_rsa_gitlab that can connect to gitlab."
+  exit 1
+fi
+if [ -f "$HOME/.ssh/id_rsa_gitlabaa" ]; then
+  echo "Using key file ~/.ssh/id_rsa_gitlab for gitlab access."
+  KEY_FILE_CONTENTS=$(cat $HOME/.ssh/id_rsa_gitlab)
+fi
+: ${GITLAB_SSH_KEY:=$KEY_FILE_CONTENTS}
+
+ssh "${opts[@]}" ${user}@$ip <<EOF
+echo "${GITLAB_SSH_KEY}" > ~/.ssh/id_rsa_gitlab
+chmod go-rwx ~/.ssh/id_rsa_gitlab
+touch ~/.ssh/config
+sed -i.bak '/### bootstrap start ###/,/### bootstrap end ###/d' ~/.ssh/config
+echo "### bootstrap start ###" >> ~/.ssh/config
+echo "Host gitlab.eng.vmware.com" >> ~/.ssh/config
+echo "StrictHostKeyChecking no" >> ~/.ssh/config
+echo "IdentityFile ~/.ssh/id_rsa_gitlab" >> ~/.ssh/config
+echo "### bootstrap end ###" >> ~/.ssh/config
+EOF
