d3d4da01c80a87bad8544fd45c5d434514c1c7df
John Gardner <huxoll@gmail.com>
2018-07-03 11:33:56 -0500

    Add download script.

---
+++ downloads/download.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+if [ ! -f VMware-ovftool-4.3.0-7948156-lin.x86_64.bundle ]; then
+  curl -L  -o VMware-ovftool-4.3.0-7948156-lin.x86_64.bundle "https://www.dropbox.com/s/n5pepfatetp55q2/VMware-ovftool-4.3.0-7948156-lin.x86_64.bundle?dl=1"
+fi
