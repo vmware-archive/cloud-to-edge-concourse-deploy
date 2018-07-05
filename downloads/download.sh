0fbbb0db45b8c77428aacf23b695951c880b3742
John Gardner <huxoll@gmail.com>
2018-07-05 16:12:54 -0500

    Add download of ova.

--- downloads/download.sh
+++ downloads/download.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 if [ ! -f VMware-ovftool-4.3.0-7948156-lin.x86_64.bundle ]; then
-  curl -L  -o VMware-ovftool-4.3.0-7948156-lin.x86_64.bundle "https://www.dropbox.com/s/n5pepfatetp55q2/VMware-ovftool-4.3.0-7948156-lin.x86_64.bundle?dl=1"
+  curl -L -o VMware-ovftool-4.3.0-7948156-lin.x86_64.bundle "https://www.dropbox.com/s/n5pepfatetp55q2/VMware-ovftool-4.3.0-7948156-lin.x86_64.bundle?dl=1"
+fi
+
+if [ ! -f xenial-server-cloudimg-amd64.ova ]; then
+  curl -L -o xenial-server-cloudimg-amd64.ova https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64.ova
 fi
