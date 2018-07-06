bdf6544965679aa89cfe4b25b5b8a252c73e491d
John Gardner <huxoll@gmail.com>
2018-07-05 23:17:57 -0500

    More role fixing.

--- functions/copy_roles.sh
+++ functions/copy_roles.sh
@@ -4,10 +4,8 @@ function copy_roles {
 
   # TODO: make this more elegant and wildcard it.
   mkdir -p skyway-automation/roles
-  cp -r ansible-role-azure-iot skyway-automation/roles/azure-iot
-  cp -r ansible-role-azure-iot skyway-automation/roles/azure-edge
-  cp -r ansible-role-azure-iot skyway-automation/roles/azure-edge
-  cp -r ansible-role-azure-iot skyway-automation/roles/azure-edge
+  cp -r ansible-role-azure-iot skyway-automation/roles/vmware.azure-iot
+  cp -r ansible-role-azure-edge skyway-automation/roles/vmware.azure-edge
   cp -r ansible-aws-greengrass/roles/awscli skyway-automation/roles/awscli
   cp -r ansible-aws-greengrass/roles/greengrass-init skyway-automation/roles/greengrass-init
   cp -r ansible-aws-greengrass/roles/greengrass-core skyway-automation/roles/greengrass-core
