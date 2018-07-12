aee68e107a80b02846f8389103ee4f1fe4629904
John Gardner <huxoll@gmail.com>
2018-07-12 13:23:57 -0500

    Generate dynamic playbook from task.yml args.

--- functions/copy_roles.sh
+++ functions/copy_roles.sh
@@ -6,10 +6,10 @@ function copy_roles {
   mkdir -p skyway-automation/roles
   cp -r ansible-role-azure-iot skyway-automation/roles/vmware.azure-iot
   cp -r ansible-role-azure-edge skyway-automation/roles/vmware.azure-edge
-  cp -r ansible-aws-greengrass/roles/awscli skyway-automation/roles/awscli
-  cp -r ansible-aws-greengrass/roles/greengrass-init skyway-automation/roles/greengrass-init
-  cp -r ansible-aws-greengrass/roles/greengrass-core skyway-automation/roles/greengrass-core
-  cp -r ansible-aws-greengrass/roles/greengrass-deploy skyway-automation/roles/greengrass-deploy
-  cp -r ansible-aws-greengrass/roles/greengrass-lambda skyway-automation/roles/greengrass-lambda
+  cp -r ansible-aws-greengrass/roles/awscli skyway-automation/roles/vmware.awscli
+  cp -r ansible-aws-greengrass/roles/greengrass-init skyway-automation/roles/vmware.greengrass-init
+  cp -r ansible-aws-greengrass/roles/greengrass-core skyway-automation/roles/vmware.greengrass-core
+  cp -r ansible-aws-greengrass/roles/greengrass-deploy skyway-automation/roles/vmware.greengrass-deploy
+  cp -r ansible-aws-greengrass/roles/greengrass-lambda skyway-automation/roles/vmware.greengrass-lambda
   ansible-galaxy install huxoll.azure-cli
 }
--- functions/generate_playbook.py
+++ functions/generate_playbook.py
@@ -2,28 +2,35 @@
 
 import sys, os
 import re
+import argparse
 
 playbook = "task-playbook.yml"
+hosts = []
 
-def emit_var(key, value, vars_file):
-    vars_file.write("%s: %s\n" % (key.lower(), value))
+def emit_role(host, role, playbook_file):
+    if host not in hosts:
+        # Assume hosts are ordered, i.e. no roles assigned to the one host
+        # after assigning to a different host.
+        hosts.append(host)
+        playbook_file.write("- hosts: %s\n" % host)
+        playbook_file.write("  roles:\n")
+    playbook_file.write("    - role: %s\n" % role)
 
 def generate_playbook(args=None):
     playbook_file = open(playbook, "w")
     playbook_file.write("---\n")
-    #- hosts: azure-driver
-    #roles:
-    #- { role: vmware.azure-iot }
 
+    hosts = []
     for hosts_and_role in args:
-        print ("%s" % hosts_and_role)
+        print (">> %s" % hosts_and_role)
         vals = re.split('=', hosts_and_role)
         if len(vals) == 2:
-            playbook_file.write("- hosts: %s\n" % vals[0])
-            playbook_file.write("  roles:\n")
-            playbook_file.write("    - role: %s\n" % vals[1])
-
+            emit_role(vals[0], vals[1], playbook_file)
     playbook_file.close()
 
 if __name__ == '__main__':
-    generate_playbook(sys.argv)
+    parser = argparse.ArgumentParser(description='System deployer.')
+    parser.add_argument('--host-role', dest='host_role', action='append',
+                        help='associate an Ansible role with a host')
+    args = parser.parse_known_args()[0]
+    generate_playbook(args.host_role)
