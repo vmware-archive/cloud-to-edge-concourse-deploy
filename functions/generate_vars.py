a8703c5556a269f5036de7a795b3ebbfc2b82edd
John Gardner <huxoll@gmail.com>
2018-07-27 10:08:55 -0500

    Fix multiline variables.

--- functions/generate_vars.py
+++ functions/generate_vars.py
@@ -5,8 +5,22 @@ import re
 
 group_var_all = "group_vars/all/vars.yml"
 
+# These are multiline variables, need to be formatted correctly as yaml.
+# There will be some sort of whitespace, explicity change that whitespace to
+# newline and correct indentation.
+force_multiline = [
+    "skyway_edge_vm_ssh_priv_key",
+    "skyway_edge_vm_ssh_pub_key"
+]
+
 def emit_var(key, value, vars_file):
-    vars_file.write("%s: %s\n" % (key.lower(), value))
+    if key in force_multiline:
+        lines = value.split('\n')
+        vars_file.write("%s: |\n" % key.lower())
+        for line in lines:
+            vars_file.write("  %s\n" % line)
+    else:
+        vars_file.write("%s: %s\n" % (key.lower(), value))
 
 def generate_vars(args=None):
     if not os.path.exists(os.path.dirname(group_var_all)):
@@ -16,15 +30,15 @@ def generate_vars(args=None):
     for k, v in os.environ.items():
         if re.search("^skyway", k, re.IGNORECASE):
             emit_var(k, v, group_vars)
-        if re.search("^aws", k, re.IGNORECASE):
+        elif re.search("^aws", k, re.IGNORECASE):
             emit_var(k, v, group_vars)
-        if re.search("^azure", k, re.IGNORECASE):
+        elif re.search("^azure", k, re.IGNORECASE):
             emit_var(k, v, group_vars)
-        if re.search("^greengrass", k, re.IGNORECASE):
+        elif re.search("^greengrass", k, re.IGNORECASE):
             emit_var(k, v, group_vars)
-        if re.search(".*s3.*", k, re.IGNORECASE):
+        elif re.search(".*s3.*", k, re.IGNORECASE):
             emit_var(k, v, group_vars)
-        if re.search(".*lambda.*", k, re.IGNORECASE):
+        elif re.search(".*lambda.*", k, re.IGNORECASE):
             emit_var(k, v, group_vars)
     group_vars.close()
 
