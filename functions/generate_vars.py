d2e3b2dc003e2627ae967ef3d2e49d0daa20d201
John Gardner <huxoll@gmail.com>
2018-08-21 11:06:26 -0500

    Add comments, cloud providers vars.

--- functions/generate_vars.py
+++ functions/generate_vars.py
@@ -28,11 +28,17 @@ def generate_vars(args=None):
     group_vars = open(group_var_all, "w")
     group_vars.write("---\n")
     for k, v in os.environ.items():
+        # Emit our own variables
         if re.search("^skyway", k, re.IGNORECASE):
             emit_var(k, v, group_vars)
+        # Emit variables for cloud providers
         elif re.search("^aws", k, re.IGNORECASE):
             emit_var(k, v, group_vars)
         elif re.search("^azure", k, re.IGNORECASE):
+            emit_var(k, v, group_vars)
+        elif re.search("^google", k, re.IGNORECASE):
+            emit_var(k, v, group_vars)
+        elif re.search("^ibm", k, re.IGNORECASE):
             emit_var(k, v, group_vars)
         elif re.search("^greengrass", k, re.IGNORECASE):
             emit_var(k, v, group_vars)
@@ -40,6 +46,8 @@ def generate_vars(args=None):
             emit_var(k, v, group_vars)
         elif re.search(".*lambda.*", k, re.IGNORECASE):
             emit_var(k, v, group_vars)
+        # Some special cases; variables that aren't named according to
+        # convention but that we need anyway.
         elif re.search("base_.*", k, re.IGNORECASE):
             emit_var(k, v, group_vars)
         elif re.search("binary_.*", k, re.IGNORECASE):
