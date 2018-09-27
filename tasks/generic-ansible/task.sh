0b974f6aab9296b16f2885ac832e9f0b864d7d27
John Gardner <huxoll@gmail.com>
2018-09-27 16:37:35 -0500

    Need git for git resource...

--- tasks/generic-ansible/task.sh
+++ tasks/generic-ansible/task.sh
@@ -45,6 +45,8 @@ echo ""
 if [ -d ../scratch-out ]; then
   cd ..
   git --version || (apt-get update -qq && apt-get install -y git)
+  git config --global user.email "nobody@vmware.com"
+  git config --global user.name "Skyway Pipeline"
   git clone scratch scratch-out
   cp automation/my-env scratch-out/task-env
   cp automation/my-args scratch-out/task-args
